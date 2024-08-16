5477662bd965eead7bfc80244096f8483a12f362
Annawhite20 <annawutang80@gmail.com>
2024-08-16 14:13:00 +0100

    first commit

---
+++ Class1.py
@@ -0,0 +1,88 @@
+#print("Hello, world")
+
+#if 5 < 10:
+    #print("Hello!")
+#else:
+ #   print("Goodbye")
+ # x = 5
+ # y = "hello world"
+ # print(x)
+ # print(y)
+
+# x = 200 # int type
+ #y = "hello class" # string type
+# print x
+# print y
+
+    # single & double Quote
+#x = 'hello world'
+#y = "hello class"
+#print(x)
+#print(y)
+
+# Assign Multiple Value
+# x, y, z, = "Orange", "Banana", "Cherry"
+# print(z)
+# print(x)
+# print(y)
+
+#x = y = z  = "Orange"
+#print(z)
+#print(x)
+#print(y)
+
+#Unpack a list
+
+#className = ["anna", "fawaz", "me"]
+#m, n, p = className
+#print(p)
+#print(m)
+#print(n)
+#x = 5
+#print(5)
+
+#x = "jet"
+#y = "fan"
+#z = "zip"
+#print(x + y + z)
+
+#x = 5
+#y = 10
+#print(x + y)
+
+#Global variables
+#x = "class!" #var
+
+#def var(): #function
+    #print("my first python " + x)
+#var()
+
+#print("print is" + x)
+
+#x = "awesome"
+
+#def myfunc():
+    #x = "fantastic"
+    #print("python is " + x)
+
+    #myfunc()
+
+    #print("python is " + x)
+
+# def myfunc():
+#     global x
+#     x = "Anna"
+# myfunc()
+# print("my name is " + x)
+# y = "awesome"
+
+# def myfunc():*
+#     global x
+#     x = "fantastic"
+# myfunc()
+# print("python is " + y)
+
+
+age = 36
+txt = f"My name is Anna,I am {age}"
+print(txt)
